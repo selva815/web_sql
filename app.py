3766ae0fa4e90d5543e0f0634737025230eadc53
selva815 <136146608+selva815@users.noreply.github.com>
2024-05-18 12:00:23 +0000

    Added html and styling
    Added html , styling and jsonify

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, jsonify
 
 app =Flask(__name__)
-
+product_id = [
+  { 'Part_number': "LFHP12021",
+    'Part_name': "Tiger",
+    'Tested_at' : "ICT-05",
+   'Failure_type': "Analog_test"
+   },
+  { 'Part_number': "LFHP12025",
+    'Part_name': "Bonnville",
+    'Tested_at' : "ICT-02",
+   'Failure_type': "Short test"
+   },
+  { 'Part_number': "LFHP4134",
+    'Part_name': "Scrambler",
+    'Tested_at' : "ICT-01",
+   'Failure_type': "Boundary scan test"
+   }
+]
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', product= product_id)
+
+
+@app.route("/api/product")
+def info():
+  return jsonify(product_id)
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True )
-
